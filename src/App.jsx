aed304c2111417f602107de726dc41ce042391b0
FreddyMaster <blackcatshd@gmail.com>
2023-04-22 00:29:43 -0400

    add alignments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import backgrounds from './data/backgrounds.json';
 import classes from './data/classes.json';
 import races from './data/races.json'
+import alignments from './data/alignments.json';
 
 
 function App() {
@@ -95,8 +96,9 @@ function App() {
         </select>
       </div>
       <div id="Class" className="tabcontent">
+        <label htmlFor="class">Class:</label>
         {/* Class tab content */}
-        <select id="class-select">
+        <select id="class">
           {classes.map(Class => (
             <option value={Class.name} key={Class.id}>
               {Class.name}
@@ -108,30 +110,38 @@ function App() {
         {/* Ability Scores / Feats tab content */}
         <label htmlFor="strength">Strength:</label>
         <input type="number" id="strength" name="Strength" min="1" value={abilityScores[0].score} onChange={updateModifier} />
-        <input type="modifier" id="strength-modifier" name="strength-modifier" value={abilityScores[0].modifier} readOnly /><br />
+        <input type="text" id="strength-modifier" name="strength-modifier" value={abilityScores[0].modifier} readOnly /><br />
 
         <label htmlFor="dexterity">Dexterity:</label>
         <input type="number" id="dexterity" name="Dexterity" min="1" value={abilityScores[1].score} onChange={updateModifier} />
-        <input type="modifier" id="dexterity-modifier" name="dexterity-modifier" value={abilityScores[1].modifier} readOnly /><br />
+        <input type="text" id="dexterity-modifier" name="dexterity-modifier" value={abilityScores[1].modifier} readOnly /><br />
 
         <label htmlFor="constitution">Constitution:</label>
         <input type="number" id="constitution" name="Constitution" min="1" value={abilityScores[2].score} onChange={updateModifier} />
-        <input type="modifier" id="constitution-modifier" name="constitution-modifier" value={abilityScores[2].modifier} readOnly /><br />
+        <input type="text" id="constitution-modifier" name="constitution-modifier" value={abilityScores[2].modifier} readOnly /><br />
 
         <label htmlFor="intelligence">Intelligence:</label>
         <input type="number" id="intelligence" name="Intelligence" min="1" value={abilityScores[3].score} onChange={updateModifier} />
-        <input type="modifier" id="intelligence-modifier" name="intelligence-modifier" value={abilityScores[3].modifier} readOnly /><br />
+        <input type="text" id="intelligence-modifier" name="intelligence-modifier" value={abilityScores[3].modifier} readOnly /><br />
 
         <label htmlFor="wisdom">Wisdom:</label>
         <input type="number" id="wisdom" name="Wisdom" min="1" value={abilityScores[4].score} onChange={updateModifier} />
-        <input type="modifier" id="wisdom-modifier" name="wisdom-modifier" value={abilityScores[4].modifier} readOnly /><br />
+        <input type="text" id="wisdom-modifier" name="wisdom-modifier" value={abilityScores[4].modifier} readOnly /><br />
 
         <label htmlFor="charisma">Charisma:</label>
         <input type="number" id="charisma" name="Charisma" min="1" value={abilityScores[5].score} onChange={updateModifier} />
-        <input type="modifier" id="charisma-modifier" name="charisma-modifier" value={abilityScores[5].modifier} readOnly /><br />
+        <input type="text" id="charisma-modifier" name="charisma-modifier" value={abilityScores[5].modifier} readOnly /><br />
       </div>
       <div id="Background" className="tabcontent">
+        <label htmlFor="background">Background:</label>
         {/* Background tab content */}
+        <select id="alignment-select">
+          {alignments.map(alignment => (
+            <option value={alignment.name} key={alignment.id}>
+              {alignment.name}
+            </option>
+          ))}
+        </select>
         <select id="background-select">
           {backgrounds.map(background => (
             <option value={background.name} key={background.id}>
@@ -139,7 +149,6 @@ function App() {
             </option>
           ))}
         </select>
-
       </div>
     </div>
   );
